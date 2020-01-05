a1289aba417fd316b586c53b44c020b83b72f7e0
pbolotin <Pavel.Bolotin@gmail.com>
2020-01-05 16:16:57 +0700

    Create base .c file for task 2

---
+++ Practice_02/practice_02.c
@@ -0,0 +1,29 @@
+/**************************************************************************
+Topic: Random tree of search, ideal balance tree of search
+***************************************************************************
+Goal:  To know how to build these trees
+***************************************************************************
+1.  Create functions for building RTS and IBTS
+***************************************************************************
+2.  Calculate middle height of trees
+    n = 10, 50, 100, 200, 400 (number of vertex in tree)
+    Fill table:
+    N  | Height RTS| Height IBTS
+    ___|___________|____________
+    10 |           |
+    50 |           |
+    100|           |
+    200|           |
+    400|           |
+    Get analize of the results
+***************************************************************************
+3.  Create function for check if binary tree is tree of search?
+    Check work of function on the builded RTS and IBTS
+***************************************************************************
+4.  Create function of search in the tree for element with the key, and
+    check its work on builded RTS and IBTS.
+***************************************************************************
+5.  Find number of operations needed for search. Compare this number
+    and height of tree.
+***************************************************************************
+
