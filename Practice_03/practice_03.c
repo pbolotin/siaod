9afcab028ee6ebf9dc40873c58db1618143d7b07
pbolotin <Pavel.Bolotin@gmail.com>
2020-03-04 23:53:07 +0700

    Start task 3

---
+++ Practice_03/practice_03.c
@@ -0,0 +1,17 @@
+/*
+* ABL-tree building
+* 1. Procedure for buid ABL-tree
+* 2. Calculate average height, cases: n=10, 50, 100, 200, 400 (how many nodes)
+* 3. Fill the table according (2) and for theorethical prognosis
+* 4. Do the experiment to calculate middle of turns for one included node.
+*/
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INT_MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
+
+int main(void) {
+	return 0;
+}
